8ea82a7f6df725743ce1e5edd86cbf793d6e14ba
Malek Sharabi <m.malek1996@hotmail.com>
2025-11-22 04:38:56 +0100

    finalized the overcollateralization invariant check

--- test/fuzz/InvariantsTest.t.sol
+++ test/fuzz/InvariantsTest.t.sol
@@ -8,14 +8,32 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract InvariantsTest is Test {
     DeployDSC deployer;
     DSCEngine engine;
     DecentralizedStableCoin dsc;
     HelperConfig config;
+    address weth;
+    address wbtc;
 
     function setUp() external {
+        deployer = new DeployDSC();
         (dsc, engine, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+        targetContract(address(engine));
+    }
+
+    function invariant_totalSupplyNeverExceedsCollateralValue() public view {
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(engine));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(engine));
+
+        uint256 wethValue = engine.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = engine.getUsdValue(wbtc, totalWbtcDeposited);
+        uint256 totalCollateralValue = wethValue + wbtcValue;
+
+        assert(totalSupply <= totalCollateralValue);
     }
 }
