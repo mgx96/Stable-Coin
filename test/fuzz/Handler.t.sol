2256e2fd7aa5b64e0f1c49de33418721d9030ed8
Malek Sharabi <m.malek1996@hotmail.com>
2026-01-20 17:25:43 +0100

    Oracle check library added and handler updated

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -84,8 +84,9 @@ contract Handler is Test {
         vm.stopPrank();
     }
 
-    function updateCollateralPrice(uint96 newPrice) public {
+    /*function updateCollateralPrice(uint96 newPrice) public {
         int256 newPriceInt = int256(uint256(newPrice));
         ethUsdPriceFeed.updateAnswer(newPriceInt);
     }
+    */
 }
